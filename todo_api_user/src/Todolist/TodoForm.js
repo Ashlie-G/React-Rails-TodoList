d818ccf96efbc89078af9cfd41be2db596f95334
Ashlie Gray <ashliejanegray@gmail.com>
2020-12-17 19:45:48 +1000

    rails api complete and started react front end components

---
+++ todo_api_user/src/Todolist/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+    
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>this is for form area</p>
+            </div>
+        )
+    }
+}
+
+export default TodoForm
